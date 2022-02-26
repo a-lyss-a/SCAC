62f909e393ef2f727c492337278b1391da520bab
nafaaal <ismailnafaal@gmail.com>
2022-02-26 15:35:22 +0000

    Remove boilterplate code

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
+// import Header from '../components/layout/Header';
+// import Footer from '../components/layout/Footer';
 
 const LayoutDefault = ({ children }) => (
   <>
-    <Header navPosition="right" className="reveal-from-bottom" />
+    {/* <Header navPosition="right" className="reveal-from-bottom" /> */}
     <main className="site-content">
       {children}
     </main>
-    <Footer />
+    {/* <Footer /> */}
   </>
 );
 
