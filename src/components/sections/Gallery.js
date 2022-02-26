8992d5e48c1d6f0d5cbb033fed4de5c690b23967
nafaaal <ismailnafaal@gmail.com>
2022-02-26 23:49:41 +0000

    hello??

--- src/components/sections/Gallery.js
+++ src/components/sections/Gallery.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 
 const Gallery = (props) => {
+  // const [countries, setCountries] = useState({ features: []});
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/gallery?query=ru").then(res => res.json()).then(data => console.log(data)).catch(function(error) {
+      console.log(error);
+  });;
+  }, []);
+
+
   return (
     <section>
-        <p>{props.country}</p>
+      {props.country}
+
     </section>
   );
 }
