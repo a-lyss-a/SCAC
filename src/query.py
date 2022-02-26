6be883d7e2359062d7fbdab967428a5f4ac839e0
Alyssa <78480394+a-lyss-a@users.noreply.github.com>
2022-02-26 21:22:20 +0000

    good luck fuckers

--- src/query.py
+++ src/query.py
@@ -20,40 +20,20 @@ def get_Image_URL(name):
         if r.get("name") == name:
             return r.get("image_url")
 
-def get_Description(name):
-    for r in response:
-        if r.get("name") == name:
-            return r.get("short_description")
-
-def get_Location(name):
-    for r in response:
-        if r.get("name") == name:
-            return r.get("location")
-
-def get_Coordinates(name):
-    for r in response:
-        if r.get("name") == name:
-            return (r.get("longitude"), r.get("latitude"))
-
-def get_Region(name):
-    for r in response:
-        if r.get("name") == name:
-            return r.get("region")
-
-def get_States(name):
-    for r in response:
-        if r.get("name") == name:
-            return r.get("states")
-
-def get_Date_Inscribed(name):
-    for r in response:
-        if r.get("name") == name:
-            return r.get("date_inscribed")
-
-def zip(country):
+def gallery(country):
     results = {}
     for n in country_sites(country):
         results[n] = get_Image_URL(n)
+    return results
+
+def details(site):
+    results = {}
+    result_keys = ("name", "image_url", "short_description", "location", "region", "states", "date_inscribed")
+    for r in response:
+        if r.get("name") == site:
+            for k in result_keys:
+                results[k] = r.get(k)
+            results["coordinates"] = (r.get("longitude"), r.get("latitude"))
     return results
 
 # def query(country):
@@ -66,20 +46,29 @@ f = open("query-request.txt", "r")
 country = f.read().strip()
 f.close()
 
+g = open("site-request.txt", "r")
+site = g.read().strip()
+g.close()
+# site = country_sites(country)[1]
 
 app = Flask(__name__)
 
 @app.route("/")
 # def hello_world():
 #     return "<p>Hello, World!</p>"
-def query():
-    # results = zip(country)
-    # html_string = ""
-    # sites = list(results.keys())
-    # pictures = list(results.values())
-    # for i in range(len(results)):
-    #     html_string += "<div><h3>{}</h3><img src='{}' alt='Image not found'></div>".format(sites[i],pictures[i])
-    # return html_string
-    results = json.dumps(zip(country))
-    #print(results)
+# def gallery_query():
+#     # results = zip(country)
+#     # html_string = ""
+#     # sites = list(results.keys())
+#     # pictures = list(results.values())
+#     # for i in range(len(results)):
+#     #     html_string += "<div><h3>{}</h3><img src='{}' alt='Image not found'></div>".format(sites[i],pictures[i])
+#     # return html_string
+#     results = json.dumps(gallery(country))
+#     #print(results)
+#     return results
+
+def details_query():
+    results = json.dumps(details(site))
+    print(results)
     return results
