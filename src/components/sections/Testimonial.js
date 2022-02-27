c4b906c851ca1c672cf02c773e2e4f23709f2614
RBan-20 <ng20035@bristol.ac.uk>
2022-02-27 02:25:22 +0000

    got things to work throughtrials

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -43,11 +43,6 @@ const Testimonial = ({
     pushLeft && 'push-left'
   );
 
-  const sectionHeader = {
-    title: 'Customer testimonials',
-    paragraph: 'HI'
-  };
-
   const monument = "Tipasa"
   const [details, setDetails] = useState([]);
   useEffect(()=>{
@@ -62,65 +57,21 @@ const Testimonial = ({
   },[])
   
   console.log(details)
+  // const sectionHeader = {
+  //   title: {details.name},
+  //   paragraph: "Hi"
+  // };
 
   return (
     <div className="container">
       <div className={innerClasses}>
-        <SectionHeader data={sectionHeader} className="center-content" />
-        <div className={tilesClasses}>
-
-          <div className="tiles-item reveal-from-right" data-reveal-delay="200">
-            <div className="tiles-item-inner">
-              <div className="testimonial-item-content">
-                <p className="text-sm mb-0">
-                  "BYE"
-                </p>
-              </div>
-              <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                <span className="testimonial-item-name text-color-high">Roman Level</span>
-                <span className="text-color-low"> / </span>
-                <span className="testimonial-item-link">
-                  <a href="#0">AppName</a>
-                </span>
-              </div>
-            </div>
+        <SectionHeader data={{title: details.name}} className="center-content" />
+          <div>
+            <img src={details.image_url}/>
+            <p className="text-sm mb-0">
+              {details.short_description}
+            </p>
           </div>
-
-          <div className="tiles-item reveal-from-bottom">
-            <div className="tiles-item-inner">
-              <div className="testimonial-item-content">
-                <p className="text-sm mb-0">
-                  — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
-                    </p>
-              </div>
-              <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                <span className="testimonial-item-name text-color-high">Diana Rynzhuk</span>
-                <span className="text-color-low"> / </span>
-                <span className="testimonial-item-link">
-                  <a href="#0">AppName</a>
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="tiles-item reveal-from-left" data-reveal-delay="200">
-            <div className="tiles-item-inner">
-              <div className="testimonial-item-content">
-                <p className="text-sm mb-0">
-                  — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
-                    </p>
-              </div>
-              <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                <span className="testimonial-item-name text-color-high">Ben Stafford</span>
-                <span className="text-color-low"> / </span>
-                <span className="testimonial-item-link">
-                  <a href="#0">AppName</a>
-                </span>
-              </div>
-            </div>
-          </div>
-
-        </div>
       </div>
     </div>
   );
