350c7b717313aea319a037e0d2d1649304a31775
Alyssa <78480394+a-lyss-a@users.noreply.github.com>
2022-02-27 05:04:47 +0000

    i'm sorry i forgot the id :(

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
-import Testimonial from './components/sections/Testimonial';
-import Gallery from './pages/Gallery';
-import MainPage from "./pages";
-import {
-  Route,
-  Switch,
-  Link,
-  Redirect
-} from "react-router-dom";
-=======
 import Details from './components/sections/Details';
 import Gallery from './components/sections/Gallery';
->>>>>>> e6e01048527cdf5de58579192f7410d3bcdf140d
 
 // Layouts
 
@@ -24,26 +12,11 @@ import Heritage from './components/HeritageSite';
 const App = () => {
 
   return (
-<<<<<<< HEAD
-    <Switch>
-       {/* <Route path="/" component={Gallery} /> */}
-       {/* <Route path="/" component={MainPage} />  */}
-      <Route  path="/Globe/Earth">
-      <Earth /> 
-      </Route>
-      <Route  path="/Gallery">
-      <Gallery /> 
-      </Route> 
-       
-    </Switch>
-
-=======
     <div>
     {/* <Earth />  */}
     {/* <Gallery country="Lithuania" />  */}
       <Details  />
     </div>
->>>>>>> e6e01048527cdf5de58579192f7410d3bcdf140d
   );
 }
 
